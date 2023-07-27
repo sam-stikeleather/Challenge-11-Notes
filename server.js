cfa5336cd61cd9b36fcf346259780b24d61218bc
Sam <sam.stikeleather@gmail.com>
2023-07-27 14:24:16 -0400

    set up to point to database file

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const jsonuuid = require('json-uuid');
+
+const database = 'db.json';
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
